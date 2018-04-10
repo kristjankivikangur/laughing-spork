b773aa072e013a94bbcf90ef8c9f198d26405970
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2018-04-10 18:53:21 +0300

    Create Tervitaja class with Tervita method

---
+++ TereMaailm/TereMaailm/Tervitaja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TereMaailm
+{
+	internal class Tervitaja
+	{
+		/// <summary>
+		/// Kuvab ekraanile tervitus teksti
+		/// </summary>
+		/// <param name="keda">nimi, keda tervitatakse</param>
+		internal static void Tervita(string keda)
+		{
+			Console.WriteLine("Tere {0}.", keda);
+		}
+	}
+}
